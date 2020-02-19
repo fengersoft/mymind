e98691cd92f4f16f23cd99b95f1765f284265d98
QinXiaofeng <473966678@qq.com>
2020-02-19 11:17:36 +0800

    增加工具栏处理

---
+++ src/mindmapwidgettoolbar.cpp
@@ -0,0 +1,24 @@
+﻿#include "mindmapwidgettoolbar.h"
+#include "ui_mindmapwidgettoolbar.h"
+
+MindMapWidgetToolBar::MindMapWidgetToolBar(QWidget* parent)
+    : QWidget(parent)
+    , ui(new Ui::MindMapWidgetToolBar)
+{
+    ui->setupUi(this);
+}
+
+MindMapWidgetToolBar::~MindMapWidgetToolBar()
+{
+    delete ui;
+}
+
+void MindMapWidgetToolBar::paintEvent(QPaintEvent* event)
+{
+    QPainter painter;
+    painter.begin(this);
+    painter.fillRect(rect(), QBrush(QColor(252, 252, 252)));
+    painter.setPen(QColor(240, 240, 240));
+    painter.drawLine(0, height(), width(), height());
+    painter.end();
+}
---
+++ src/mindmapwidgettoolbar.h
@@ -0,0 +1,23 @@
+﻿#ifndef MINDMAPWIDGETTOOLBAR_H
+#define MINDMAPWIDGETTOOLBAR_H
+
+#include <QPainter>
+#include <QWidget>
+
+namespace Ui {
+class MindMapWidgetToolBar;
+}
+
+class MindMapWidgetToolBar : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit MindMapWidgetToolBar(QWidget* parent = nullptr);
+    ~MindMapWidgetToolBar();
+    void paintEvent(QPaintEvent* event);
+
+private:
+    Ui::MindMapWidgetToolBar* ui;
+};
+
+#endif // MINDMAPWIDGETTOOLBAR_H
